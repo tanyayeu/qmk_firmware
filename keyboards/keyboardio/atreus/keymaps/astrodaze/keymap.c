cef0346466004df609bef51bebd2a53172b249b1
Tanya Yeu <tanyayeu@gmail.com>
2021-03-10 18:11:52 -0800

    switch quotes

--- keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_NUM] = LAYOUT( /* [> RAISE <] */
     KC_1, KC_2,   KC_3,   KC_4,  KC_5,                            KC_6, KC_7,    KC_8,   KC_9, KC_0,
-    KC_LPRN, KC_LEFT, KC_DOWN, KC_QUES, KC_RPRN,                  KC_LEFT, KC_DOWN,  KC_UP, KC_RGHT, KC_DQUO,
+    KC_LPRN, KC_LEFT, KC_DOWN, KC_QUES, KC_RPRN,                  KC_LEFT, KC_DOWN,  KC_UP, KC_RGHT, KC_QUOT,
     KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
     KC_TILD, KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
   /*
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_FUN] = LAYOUT( /* [> LOWER <] */
     KC_LPRN,  KC_RPRN, _______,   _______,  _______,                   KC_UP,   KC_F7,   KC_F8,   KC_MINS,   KC_EQL  ,
-    KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
+    KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_DQUO  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
     KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS ),
 
