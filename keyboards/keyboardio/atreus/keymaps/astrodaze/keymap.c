c7e3aedaf1f1ffe2e0802e0f1e544baa8e8701e9
Tanya Yeu <tanyayeu@gmail.com>
2021-03-10 21:33:15 -0800

    minor changes

--- keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
@@ -16,12 +16,14 @@ enum layer_names {
 enum tap_dances {
   TD_GUI_CAPS,
   TD_Q_TAB,
+  TD_A_TAB
 };
 
 // Tap dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_GUI_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_CAPS),   // tap once for shift, twice for caps
   [TD_Q_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_TAB), //TAP FOR Q, DT FOR TAB
+  [TD_A_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_TAB), //TAP FOR A, DT FOR TAB
 };
 
 
@@ -29,9 +31,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     TD(TD_Q_TAB),    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+    TD(TD_A_TAB),    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,            KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-    KC_ESC, KC_TAB, KC_LGUI,  LT(_NUM, KC_BSPC),TD(TD_GUI_CAPS),  KC_LCPO,   KC_RAPC, LT(_SYMBOL,KC_SPC),  LT(_FUN,KC_ENT), KC_MINS, KC_QUOT, KC_ENT ),
+    KC_ESC, KC_TAB, KC_RALT,  LT(_NUM, KC_BSPC),TD(TD_GUI_CAPS),  KC_LCPO,   KC_RCPC, LT(_SYMBOL,KC_SPC),  LT(_FUN,KC_ENT), KC_MINS, KC_QUOT, KC_ENT ),
 
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -51,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
    */
   [_FUN] = LAYOUT( /* [> LOWER <] */
-    KC_LPRN,  KC_RPRN, _______,   _______,  _______,                   KC_UP,   KC_F7,   KC_F8,   KC_MINS,   KC_EQL  ,
+    KC_LPRN,  KC_RPRN, KC_LT,   KC_GT,  _______,                   KC_UP,   KC_F7,   KC_F8,   KC_MINS,   KC_EQL  ,
     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_DQUO  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS ),
+    KC_NO,   KC_VOLD, KC_LGUI, KC_DEL, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS ),
 
   [_SYMBOL] = LAYOUT( /* [> LOWER <] */
     KC_EXLM,  KC_AT, KC_HASH,   KC_DLR,  KC_PERC,                   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_UNDS,   KC_PLUS ,
