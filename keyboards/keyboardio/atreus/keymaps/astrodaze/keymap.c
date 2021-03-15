58c223a36180184b2ef288dfb3bac5f2cf7c0a0a
Tanya Yeu <tanyayeu@gmail.com>
2021-03-15 10:55:19 -0700

    atreus changes

--- keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD(TD_Q_TAB),    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     TD(TD_A_TAB),    KC_S,    KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,            KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-    KC_ESC, KC_TAB, KC_RALT,  LT(_NUM, KC_BSPC),TD(TD_GUI_CAPS),  KC_LCPO,   KC_RCPC, LT(_SYMBOL,KC_SPC),  LT(_FUN,KC_ENT), KC_MINS, KC_QUOT, KC_ENT ),
+    KC_ESC, KC_RALT, KC_LSFT,  LT(_NUM, KC_BSPC),TD(TD_GUI_CAPS),  KC_LCPO,   KC_RCPC, LT(_SYMBOL,KC_SPC),  LT(_FUN,KC_ENT), KC_MINS, KC_QUOT, KC_ENT ),
 
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1, KC_2,   KC_3,   KC_4,  KC_5,                            KC_6, KC_7,    KC_8,   KC_9, KC_0,
     KC_LPRN, KC_LEFT, KC_DOWN, KC_QUES, KC_RPRN,                  KC_LEFT, KC_DOWN,  KC_UP, KC_RGHT, KC_QUOT,
     KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    KC_TILD, KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
+    KC_TILD, KC_INS,  KC_LSFT, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
   /*
    * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
    *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
@@ -56,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LPRN,  KC_RPRN, KC_LT,   KC_GT,  _______,                   KC_UP,   KC_F7,   KC_F8,   KC_MINS,   KC_EQL  ,
     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_DQUO  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_DEL, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS ),
+    KC_NO,   KC_VOLD, KC_LSFT, KC_DEL, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS ),
 
   [_SYMBOL] = LAYOUT( /* [> LOWER <] */
     KC_EXLM,  KC_AT, KC_HASH,   KC_DLR,  KC_PERC,                   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_UNDS,   KC_PLUS ,
     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, KC_DQUO,                   _______, KC_F4,   KC_F5,   KC_MINS,   KC_EQL  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS )
+    KC_NO,   KC_VOLD, KC_LSFT, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  _______, KC_PSCR, KC_SLCK, KC_PAUS )
 };
