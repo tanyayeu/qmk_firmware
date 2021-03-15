73edb9512c7c4588da499fa272057371b637f1b7
Tanya Yeu <tanyayeu@gmail.com>
2021-03-14 17:51:51 -0700

    separate keymap for proton c

---
+++ keyboards/reviung39/keymaps/astrodaze-p/keymap.c
@@ -0,0 +1,78 @@
+/* Copyright 2019 gtips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _BASE,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+    _SYM
+};
+
+enum tap_dances {
+  TD_SFT_PRN
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_SFT_PRN] = ACTION_TAP_DANCE_DOUBLE(KC_LSPO, KC_RPRN) //HOLD FOR LSFT, TAP FOR LPRN, DT FOR RPRN
+};
+
+#define LOWER  MO(_LOWER)
+#define RAISE  MO(_RAISE)
+#define ADJUST MO(_ADJUST)
+#define SYM    MO(_SYM)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_reviung39(
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+    TD_SFT_PRN,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LCPO,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
+                                                       LT(RAISE, KC_BSPC),    LT(SYM, KC_SPC),   LT(LOWER, KC_SPC)
+  ),
+  
+  [_LOWER] = LAYOUT_reviung39(
+    KC_GESC,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
+    KC_CAPS,  KC_UNDS,  KC_PLUS,  KC_LBRC,  KC_QUES,   KC_PIPE,            KC_LEFT,  KC_LEFT,  KC_UP,    KC_RGHT,  KC_GRV,   KC_BSLS,
+    KC_LSFT,  _______,   KC_LGUI,  KC_LALT,  _______,   _______,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  KC_ENT,
+                                                       ADJUST,  SYM,   _______
+  ),
+  
+  [_RAISE] = LAYOUT_reviung39(
+    KC_TILD,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
+    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_LEFT,    KC_LEFT,    KC_UP,    KC_RGHT,    KC_F5,    KC_F6,
+    _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+                                                       _______,  SYM,  ADJUST
+  ),
+  
+  [_ADJUST] = LAYOUT_reviung39(
+    RESET,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                                       _______,  XXXXXXX,  _______
+  ),
+
+  [_SYM] = LAYOUT_reviung39(
+    RESET,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            KC_LBRC,  KC_RBRC,  XXXXXXX,  KC_MINS,  KC_EQL,  XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                                       _______,  XXXXXXX,  _______
+  ),
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
