12bb795f5513cdc5df0c647514cd17746313d4a9
Tanya Yeu <tanyayeu@gmail.com>
2021-03-17 22:06:54 -0700

    brackets

--- keyboards/reviung39/keymaps/astrodaze/keymap.c
+++ keyboards/reviung39/keymaps/astrodaze/keymap.c
@@ -24,11 +24,11 @@ enum layer_names {
 };
 
 enum tap_dances {
-  TD_QUO_RPRN
+  TD_QUO_RPRN,
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_QUO_RPRN] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_RPRN) //TAP FOR QUOTE, DT FOR RIGHT PRN
+  [TD_QUO_RPRN] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_RPRN), //TAP FOR QUOTE, DT FOR RIGHT PRN
 };
 
 // Layer names
@@ -54,20 +54,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung39(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_LSPO,  CTL_A,    ALT_S,    GUI_D,    SFT_F,      KC_G,              KC_H,     SFT_J,    GUI_K,    ALT_L,    CTL_SCLN,   TD(TD_QUO_RPRN),
-    KC_LCPO,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
+    MT(MOD_LCTL, KC_LBRC),  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
                                                        LT(RAISE, KC_BSPC),    LT(SYM, KC_SPC),   LT(LOWER, KC_ENT)
   ),
   
   [_LOWER] = LAYOUT_reviung39(
     KC_GESC,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
-    KC_CAPS,  KC_UNDS,  KC_PLUS,  KC_LBRC,  KC_QUES,   KC_PIPE,            KC_LEFT,  KC_LEFT,  KC_UP,    KC_RGHT,  KC_GRV,   KC_BSLS,
-    KC_LSFT,  _______,   KC_LGUI,  KC_LALT,  _______,   _______,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  KC_ENT,
+    KC_CAPS,  KC_RBRC,  KC_LBRC,  KC_RCBR,  KC_LCBR,   KC_PIPE,            KC_LEFT,  KC_LEFT,  KC_UP,    KC_RGHT,  KC_GRV,   KC_BSLS,
+    KC_LCBR,  _______,   KC_LGUI,  KC_LALT,  _______,   _______,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  KC_ENT,
                                                        ADJUST,  SYM,   _______
   ),
   
   [_RAISE] = LAYOUT_reviung39(
     KC_TILD,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
-    KC_CAPS,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,    KC_F5,    KC_F6,
+    KC_CAPS,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_QUES,   KC_BSLS,            KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,    KC_F5,    KC_F6,
     _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
                                                        _______,  SYM,  ADJUST
   ),
