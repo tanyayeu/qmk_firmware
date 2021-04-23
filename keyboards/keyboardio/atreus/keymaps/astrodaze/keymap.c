878f29ca332e847ccedc0842083f690be989e50f
Tanya Yeu <tanyayeu@gmail.com>
2021-04-22 17:28:43 -0700

    move ent

--- keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/astrodaze/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     CTL_A,    ALT_S,    GUI_D, SFT_F,    KC_G,                                        KC_H,    SFT_J,   GUI_K,   ALT_L,   CTL_SCLN,
     LT(_OTHER, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,                        KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    KC_ESC, KC_RALT, KC_LGUI,  LT(_FUN,KC_BSPC), LT(_NUM,KC_BSPC),  LT(_FUN,KC_TAB),     LT(_SYMBOL,KC_ESC), LT(_SYMBOL,KC_SPC),  LT(_FUN,KC_ENT), _______, KC_DEL, KC_ENT ),
+    KC_ESC, KC_RALT, KC_LGUI,  LT(_FUN,KC_BSPC), LT(_NUM,KC_BSPC),  LT(_FUN,KC_TAB),     LT(_SYMBOL,KC_ENT), LT(_SYMBOL,KC_SPC),  LT(_FUN,KC_ENT), _______, KC_DEL, KC_ENT ),
 
   [_NUM] = LAYOUT( 
     KC_1, KC_2,   KC_3,   KC_4,  KC_5,                            KC_6, KC_7,    KC_8,   KC_9, KC_0,
